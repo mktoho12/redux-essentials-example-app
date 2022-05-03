f63f007d903e276c9f66f3f0f20fe2fcbc7b0ccb
えむけー <mk@mktoho.org>
2022-05-03 18:37:11 +0900

    add user page

---
+++ features/users/User.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import { FC, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { RootState } from '../../app/store'
+import H2 from '../../components/heading/H2'
+import useIdlingPostSlice from '../../hooks/IdlingPostSlice'
+import { fetchPosts, selectAllPosts } from '../posts/postsSlice'
+import { selectUserById } from './usersSlice'
+
+type Props = {
+  id: string
+}
+
+const User: FC<Props> = ({ id }) => {
+  const user = useAppSelector((state: RootState) => selectUserById(state, id))
+
+  const dispatch = useAppDispatch()
+  const postStatus = useAppSelector((state: RootState) => state.posts.status)
+  const posts = useAppSelector(selectAllPosts)
+
+  useIdlingPostSlice()
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  return user ? (
+    <div>
+      <h1 className="text-2xl mt-4">{user.name}</h1>
+
+      <section className="p-0 my-4">
+        <H2>Posts</H2>
+        <ul>
+          {posts
+            .filter(post => post.user === user.id)
+            .map(post => (
+              <li key={post.id}>
+                <Link href={`/posts/${post.id}`}>
+                  <a>{post.title}</a>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </section>
+    </div>
+  ) : (
+    <></>
+  )
+}
+
+export default User
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
+import User from '../../features/users/User'
+import firstIfArray from '../../lib/firstIfArray'
 import { NextPageWithLayout } from '../_app'
 
 const UserPage: NextPageWithLayout = () => {
   const router = useRouter()
   const userId = router.query.id
-  return <section></section>
+  return <section>{userId && <User id={firstIfArray(userId)} />}</section>
 }
 
 export default UserPage
